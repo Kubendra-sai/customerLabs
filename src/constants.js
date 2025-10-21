59dcf8b8029739329aea5554aeda92322d1deb7e
skubendrasai <skubendrasai@gmail.com>
2025-10-21 16:00:42 +0530

    removed the unwanted code in constant.js

--- src/constants.js
+++ src/constants.js
@@ -13,6 +13,4 @@ export const DROPDOWN_LIST =
     {Label: 'State', Value: 'state',},
 ]
 
-// export const PLACEHOLDER_OBJECT = {Label: 'Add Schema to segment', Value: '',}
-
-export const SAVE_SEGMENT_URL = "https://webhook.site/c5306b85-dede-4dac-867c-82cd61f4eea3" //https://cors-anywhere.herokuapp.com/
+export const SAVE_SEGMENT_URL = "https://webhook.site/c5306b85-dede-4dac-867c-82cd61f4eea3"
